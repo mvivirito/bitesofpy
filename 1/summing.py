d4e6b94c0cb1f8e60ef352ea670988fa69aa6d24
Michael <mike@fbsdx220.miketheinventor.com>
2019-08-22 03:30:16 -0700

    Finito

--- 1/summing.py
+++ 1/summing.py
@@ -1,12 +1,15 @@
 def sum_numbers(numbers=None):
     sumOfNumbers = 0
     if numbers == None:
-        for number in range(100):
-            sumOfNumbers += number
+        for number in range(0,101):
+            sumOfNumbers = sumOfNumbers + number
+            print(sumOfNumbers) 
 
     else:
         for number in numbers: 
             sumOfNumbers += number
-    return(sumOfNumbers)
 
-    
+    return(sumOfNumbers)  
+
+
+print(sum_numbers())
