7e3f1646f8420dc825d6a5aebe2e9e99364317d0
Michael <mike@fbsdx220.miketheinventor.com>
2019-08-25 00:31:35 -0700

    finished quick

---
+++ 101/driving.py
@@ -0,0 +1,12 @@
+MIN_DRIVING_AGE = 18
+
+
+def allowed_driving(name, age):
+    """Print '{name} is allowed to drive' or '{name} is not allowed to drive'
+       checking the passed in age against the MIN_DRIVING_AGE constant"""
+    status = ""
+    if age >= 18:
+        status = name + " is allowed to drive"
+    else:
+        status = name + " is not allowed to drive"
+    return(status)
